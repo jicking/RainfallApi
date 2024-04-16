22cbc921b19d598274be83793fffdfb8887a16e8
jicking <7297029+jicking@users.noreply.github.com>
2024-04-16 18:44:00 +0800

    add response models

---
+++ RainfallApi/Models/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace RainfallApi.Models;
+
+public class ErrorResponse
+{
+    public required string Message { get; set; }
+    public required List<ErrorDetail> Details { get; set; }
+}
+
+public class ErrorDetail
+{
+    public required string PropertyName { get; set; }
+    public required string Message { get; set; }
+}
---
+++ RainfallApi/Models/RainfallReadingResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace RainfallApi.Models;
+
+public class RainfallReadingResponse
+{
+    public required List<RainfallReading> Readings { get; set; }
+}
+
+public class RainfallReading
+{
+    public DateTime DateMeasured { get; set; }
+    public decimal AmountMeasured { get; set; }
+}
