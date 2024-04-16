c3732344029254df42dce6869cab545eea2f9be1
jicking <7297029+jicking@users.noreply.github.com>
2024-04-16 18:44:12 +0800

    add controller

---
+++ RainfallApi/Controllers/RainfallController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RainfallApi.Models;
+
+namespace RainfallApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RainfallController : Controller
+{
+    [HttpGet("id/{stationId}/readings")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RainfallReadingResponse))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ErrorResponse))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorResponse))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ErrorResponse))]
+    public IActionResult GetRainfallReadings(string stationId, [FromQuery] int count = 10)
+    {
+        var readings = new List<RainfallReading>();
+        readings.Add(new RainfallReading
+        {
+            DateMeasured = DateTime.UtcNow,
+            AmountMeasured = 1
+        });
+
+        var response = new RainfallReadingResponse
+        {
+            Readings = readings
+        };
+        return Ok(response);
+    }
+}
