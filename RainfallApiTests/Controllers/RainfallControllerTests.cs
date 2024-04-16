7a5050d69635c508aece81f8c240938c0e3749e9
jicking <7297029+jicking@users.noreply.github.com>
2024-04-16 21:12:23 +0800

    add failing test

---
+++ RainfallApiTests/Controllers/RainfallControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using RainfallApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainfallApi.Controllers.Tests
+{
+    public class RainfallControllerTests
+    {
+        [Fact()]
+        public void RainfallControllerTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetRainfallReadingsAsyncTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
---
+++ RainfallApiTests/Queries/GetReadingsByStationHandlerTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using RainfallApi.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainfallApi.Queries.Tests
+{
+    public class GetReadingsByStationHandlerTests
+    {
+        [Fact()]
+        public void HandleTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
